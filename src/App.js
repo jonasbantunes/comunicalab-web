a6cf358a600360f5bce2e092d37d2adb9c0a6353
Arthur Almeida <arthurdosantosalmeida@gmail.com>
2020-02-10 16:21:14 -0300

    Tela de login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Login from './modules/Login/index';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Learn React</header>
+      <Login />
     </div>
   );
 }
