4745db0d1c87bd40222b0ce9f30b16b056183548
Jonas Antunes <jonasbantunes@gmail.com>
2021-01-14 14:38:03 -0300

    fix(navbar): Melhora design do menu da barra de navegação

--- src/utils/components/Menu.js
+++ src/utils/components/Menu.js
@@ -1,55 +1,53 @@
 import React from 'react';
-import '../styles/Menu.css';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Menu.module.css';
 
 function Menu() {
   return (
-    <div className="menu">
-      <ul className="menuMenu">
-        <li className="menuSubMenu">
-          <p> Laboratório </p>
-          <ul>
-            <li className="menuSubSubMenu">
-              <a href="/Laboratorio/Registrar"> Registrar </a>
-            </li>
-            <li className="menuSubSubMenu">
-              <a href="/Laboratorio/Listar"> Listar </a>
-            </li>
-          </ul>
-        </li>
-        <li className="menuSubMenu">
-          <p> Equipamento </p>
-          <ul>
-            <li className="menuSubSubMenu">
-              <a href="/Equipamento/Registrar"> Registrar</a>
-            </li>
-            <li className="menuSubSubMenu">
-              <a href="/Equipamento/Listar"> Listar </a>
-            </li>
-          </ul>
-        </li>
-        <li className="menuSubMenu">
-          <p> Software </p>
-        </li>
-        <li className="menuSubMenu">
-          <p> Chamados </p>
-        </li>
-        <li className="menuSubMenu">
-          <p> Imagens </p>
-
-          <ul>
-            <li className="menuSubSubMenu">
-              <a href="/Imagens/Registrar"> Registrar</a>
-            </li>
-            <li className="menuSubSubMenu">
-              <a href="/Imagens/Listar"> Listar </a>
-            </li>
-          </ul>
-        </li>
-        <li className="menuSubMenu">
-          <p> Usuário </p>
-        </li>
-      </ul>
-    </div>
+    <ul className={styles.menu}>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Laboratório</p>
+        <ul className={styles.dropdown}>
+          <li className={styles.dropdownItem}>
+            <Link className={styles.dropdownLink} to="/Laboratorio/Registrar">
+              Cadastrar
+            </Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link className={styles.dropdownLink} to="/Laboratorio/Listar">
+              Listar Laboratórios
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Equipamento</p>
+        <ul className={styles.dropdown}>
+          <li className={styles.dropdownItem}>
+            <Link className={styles.dropdownLink} to="/Equipamento/Registrar">
+              Cadastrar
+            </Link>
+          </li>
+          <li className={styles.dropdownItem}>
+            <Link className={styles.dropdownLink} to="/Equipamento/Listar">
+              Listar Equipamentos
+            </Link>
+          </li>
+        </ul>
+      </li>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Software</p>
+      </li>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Chamados</p>
+      </li>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Imagens</p>
+      </li>
+      <li className={styles.menuItem}>
+        <p className={styles.menuItemName}>Usuário</p>
+      </li>
+    </ul>
   );
 }
 
